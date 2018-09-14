04d1ce32f0a036601146f4ac300caae293270869
Monica  Karasek <mlkarasek@gmail.com>
2018-09-14 21:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn()
+};
+
+function setThisWithApply(fn, thisValue, args) {
+
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+
+}
